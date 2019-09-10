8dfad5ab44bee795a4b5d0cde90853a7a19b4e61
Kristian <bobish0@users.noreply.github.com>
2019-09-10 16:30:57 +0200

    Add files via upload

---
+++ grupp 7 landing page/js/javascript.js
@@ -0,0 +1,16 @@
+
+    $('html, body').animate({
+        scrollTop: $("#section1").offset().top
+    }, 1000);
+
+    $('html, body').animate({
+        scrollTop: $("#section2").offset().top
+    }, 1000);
+
+    $('html, body').animate({
+        scrollTop: $("#section3").offset().top
+    }, 1000);
+
+    $('html, body').animate({
+        scrollTop: $("#section4").offset().top
+    }, 1000);
